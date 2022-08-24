5faf812b8e4072b805894413f28aa7c5fab77631
Deniz Dogan <denizdogan@users.noreply.github.com>
2022-08-24 02:16:16 +0200

    Add Netlify provider (#9)

---
+++ services/netlify.ts
@@ -0,0 +1,25 @@
+// https://docs.netlify.com/configure-builds/environment-variables/
+
+import IDetectProvider from "../detectProvider.ts";
+
+const netlifyProvider: IDetectProvider = {
+  detect(env) {
+    return env.NETLIFY === "true";
+  },
+  configuration(env) {
+    const isPr = env.PULL_REQUEST === "true";
+
+    return {
+      name: "Netlify",
+      service: "netlify",
+      commit: env.COMMIT_REF,
+      build: env.BUILD_ID,
+      buildUrl: env.DEPLOY_URL,
+      branch: env.BRANCH,
+      jobUrl: env.DEPLOY_URL,
+      isPr,
+    };
+  },
+};
+
+export default netlifyProvider;
